aa80d62c86b72f73ff74f486b483b5b3a05104e9
ServoDev <ryansblachowicz@gmail.com>
2026-02-28 18:58:10 -0500

    term options

--- init.lua
+++ init.lua
@@ -1,8 +1,14 @@
+-- github.com/ServoDev
+-- important default keybinds :
+-- shift+k              read info about function under cursor
+-- cntrl+w cntrl + d    read diagnostic info under cursor
+
 -- vim options
 vim.o.relativenumber = true
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.o.winborder = "rounded"
+vim.g.netrw_banner = 0
 vim.opt.path:append('**') -- enables easier searching for files
 
 -- disable comment continuation ; wrapped up to ovoid overide
@@ -11,6 +17,14 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = function()
     vim.opt_local.formatoptions:remove({ "c", "r", "o" })
   end,
+})
+
+-- exit terminal mode with cntrl w
+vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], { noremap = true })
+-- automatically enter terminal mode when entering a terminal buff
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "term://*",
+  command = "startinsert",
 })
 
   
