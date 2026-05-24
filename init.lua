8b2e4488512278949147299538c81207b7507ac7
Ryan Blachowicz <ryansblachowicz@gmail.com>
2026-05-24 14:18:19 -0400

    tree sitter and java

--- init.lua
+++ init.lua
@@ -13,6 +13,13 @@ vim.o.winborder = "rounded"
 vim.g.netrw_banner = 0
 vim.opt.path:append('**') -- enables easier searching for files
 
+-- treat .ino as cpp file
+vim.filetype.add({
+  extension = {
+    ino = "cpp",
+  },
+})
+
 
 -- disable comment continuation ; wrapped up to ovoid overide
 vim.api.nvim_create_autocmd("FileType", {
@@ -30,23 +37,53 @@ vim.api.nvim_create_autocmd("BufEnter", {
   command = "startinsert",
 })
 
-  
--- colorscheme
+-- plugins
 vim.pack.add({
+        -- gruvbox scheme
 	{src = "https://github.com/ellisonleao/gruvbox.nvim"},
-	{src = "https://github.com/neovim/nvim-lspconfig"}
+        -- lsp defaults
+	{src = "https://github.com/neovim/nvim-lspconfig"},
+        -- treesitter
+        {src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 })
-vim.cmd("colorscheme gruvbox");
 
--- treat ino as cpp
-vim.filetype.add({
-  extension = {
-    ino = "cpp",
+-- treesitter
+require 'nvim-treesitter.config'.setup {
+  ensure_installed = {
+    "java",
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "markdown",
+    "markdown_inline",
+    "go"
   },
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true
+  }
+}
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "java", "lua", "c", "cpp", "go" },
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
 })
+
+
+-- colorscheme
+vim.cmd("colorscheme gruvbox");
 
 -- clangd lsp
 vim.lsp.enable({"clangd"})
+
+-- java lsp
+vim.lsp.enable({"java_language_server"})
 
 
 -- gd jumps to decleration rather than definition when using lsp
